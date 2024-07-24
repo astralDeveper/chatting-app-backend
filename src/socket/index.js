32b8bc220726af5f8f2cb4c5b4c6dfbddc9f59ef
Astral Developer <astraldevelopers0@gmail.com>
2024-07-25 04:12:53 +0500

    profile viwe req

--- src/socket/index.js
+++ src/socket/index.js
@@ -5,12 +5,14 @@ const {
   getOnlineReceipt,
   insertChat,
 } = require("./socketFunctions.js");
+const User = require("../models/User.js"); // Ensure you have this model imported
 
 const socketServer = (io) => {
   io.use(socketMiddleware);
 
   io.on("connection", (socket) => {
     console.log("new connection");
+
     socket.on("connected", async (data) => {
       await setUserOnline({ userId: data.id, socketId: socket.id });
     });
@@ -26,13 +28,42 @@ const socketServer = (io) => {
 
       let sender = await getOnlineReceipt({ userId: data?.from });
 
-      
       if (receipt) {
         console.log("sending message");
         io.to(receipt.socketId).emit("new-message", message);
       }
-      
+
       io.to(sender.socketId).emit("message-delivered", message);
+    });
+
+    socket.on("send-profile-view-request", async (data) => {
+      try {
+        const { fromUserId, toUserId } = data;
+
+        // Find the target user
+        const targetUser = await User.findById(toUserId);
+        if (!targetUser) {
+          return socket.emit("error", { message: "Target user not found" });
+        }
+
+        // Add the requesting user's ID to the profileViewRequests array if not already present
+        if (!targetUser.profileViewRequests.includes(fromUserId)) {
+          targetUser.profileViewRequests.push(fromUserId);
+          await targetUser.save();
+
+          // Emit an event to notify the target user
+          const recipient = await getOnlineReceipt({ userId: toUserId });
+          if (recipient) {
+            io.to(recipient.socketId).emit("profile-view-request", {
+              message: "You have a new profile view request",
+              from: fromUserId,
+            });
+          }
+        }
+      } catch (error) {
+        console.error(error.message);
+        socket.emit("error", { message: error.message });
+      }
     });
 
     socket.on("disconnect", async () => {
