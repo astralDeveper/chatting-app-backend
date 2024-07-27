f0ea2861cc5475f6f3ab232cb435057ea3ec2587
Astral Developer <astraldevelopers0@gmail.com>
2024-07-27 05:10:50 +0500

    set

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -267,7 +267,7 @@ const GetConversations = async (req, res) => {
 
     let conversations = await Conversation.find({
       participants: { $all: [auth?._id] },
-    }).populate({ path: "participants", select: " name _id image displayname" });
+    }).populate({ path: "participants", select: " name _id image displayName" });
     return res.status(200).json({ conversations, status: false });
   } catch (error) {
     return res.status(500).json({ message: error?.message, status: false });
